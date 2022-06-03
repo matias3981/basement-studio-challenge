cb60f103a0c56dd713351e4dec995d5a87984599
mtorreblanca <matias3981@gmail.com>
2022-06-03 10:40:58 -0300

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+import GlobalStyles from '../components/global-styles'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (<>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>)
+}
+
+export default MyApp
